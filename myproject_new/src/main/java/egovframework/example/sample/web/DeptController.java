1c28e3ab6bc71c1fd1567769d68c6e2ac9b17e18
DESKTOP-M0321IK\HOME <wj402@naver.com>
2024-05-01 22:17:58 +0900

    [Feat] SQL설정
    1. SQL설정으로 오라클 DB를 연결했습니다.
    1-1. Dept_SQL.xml
    1-2. sql-map-config.xml
    1-3. 컨트롤로서 서비스 설정 하고 만든후 실행시킴
    
    #Resolves: #1

--- myproject_new/src/main/java/egovframework/example/sample/web/DeptController.java
+++ myproject_new/src/main/java/egovframework/example/sample/web/DeptController.java
@@ -1,12 +1,18 @@
 package egovframework.example.sample.web;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import egovframework.example.sample.service.DeptService;
 import egovframework.example.sample.service.DeptVO;
 
 @Controller
 public class DeptController {
+	
+	@Resource(name="deptService")
+	private DeptService deptService;
 	
 	@RequestMapping(value="/deptWrite.do")
 	public String deptWrite() {
@@ -15,11 +21,19 @@ public class DeptController {
 	}
 	
 	@RequestMapping(value="/deptWriteSave.do")
-	public String InsertDept(DeptVO vo) {
+	public String InsertDept(DeptVO vo) throws Exception {
 		
 		System.out.println(vo.getDeptno());
 		System.out.println(vo.getDname());
 		System.out.println(vo.getLoc());
+		
+		// 서비스 설정 하고 만든 후 한번에 실행시킨다
+		String result = deptService.InsertDept(vo);
+		if( result == null ) {
+			System.out.println("저장완료");
+		} else {
+			System.out.println("저장실패");
+		}
 		
 		return "";
 	}
