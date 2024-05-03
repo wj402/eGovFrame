1161bc4ec3bcd36a47a6674997a3da44c7dba99c
DESKTOP-M0321IK\HOME <wj402@naver.com>
2024-05-04 03:27:08 +0900

    [Init] 프로젝트 연동
    1. 프로젝트를 시작하기전에 패키지를 만들어 연동하는 것을 기본적으로 했습니다.
    1-1. 새로운 main 패키지에 컨트롤러 생성 해서 웹 매핑
    1-2. dispatcher-servlet.xml, context-common.xml에 들어가 새로운 패키지 만든것을 egoveframework라 쓰인데에 main이라고 패키지 추가
    
    #Resolves: #1

---
+++ myproject_new/src/main/java/main/web/BoardController.java
@@ -0,0 +1,14 @@
+package main.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController {
+
+	@RequestMapping("/boardWrite.do")
+	public String boardWrite() {
+		
+		return "board/boardWrite";
+	}
+}
