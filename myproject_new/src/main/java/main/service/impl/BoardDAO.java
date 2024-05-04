6a204019116a5f1f130aea8022964de4463583a5
DESKTOP-M0321IK\HOME <wj402@naver.com>
2024-05-04 19:30:08 +0900

    [Feat] 게시판 화면목록 구현
    1. 게시판 화면 목록을 만들었습니다. 서비스, 서비스 인터페이스 , DB연결, 쿼리작성을 했습니다.
    
    #Resolves: #1

--- myproject_new/src/main/java/main/service/impl/BoardDAO.java
+++ myproject_new/src/main/java/main/service/impl/BoardDAO.java
@@ -1,5 +1,7 @@
 package main.service.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import egovframework.rte.psl.dataaccess.EgovAbstractDAO;
@@ -10,6 +12,11 @@ public class BoardDAO extends EgovAbstractDAO {
 
 	public String insertNBoard(BoardVO vo) {
 		return (String) insert("boardDAO.insertNBoard", vo);
+	}
+
+	public List<?> selectNBoardList(BoardVO vo) {
+
+		return list("boardDAO.selectNBoardList", vo);
 	}
 	
 	
